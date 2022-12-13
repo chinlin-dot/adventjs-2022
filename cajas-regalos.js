8010afedd3fa54c93b231e7c66dc81c29158c294
chinlin-dot <eliiyaa98@gmail.com>
2022-12-12 20:27:51 -0600

    challenges 3

---
+++ cajas-regalos.js
@@ -0,0 +1,13 @@
+
+const packOfGifts = ["book", "doll", "ball"]
+const reindeers = ["dasher", "dancer"]
+
+// el pack de regalos pesa 4 + 4 + 4 = 12
+// los renos pueden llevar (2 * 6) + (2 * 6) = 24
+// por lo tanto, Santa Claus puede entregar 2 cajas de regalos
+
+function distributeGifts(packOfGifts, reindeers) {
+  return 0
+}
+
+distributeGifts(packOfGifts, reindeers) // 2
